b3157e9c996033ae4842c0c93a90e5d6620c7648
appalrpivotal <appallaravi@gmail.com>
2015-11-09 15:28:51 -0500

    Changes for blue green deployment

--- flydbmigrate/target/classes/db/migration/V2__import.sql
+++ flydbmigrate/target/classes/db/migration/V2__import.sql
@@ -1,1 +1,8 @@
-insert into companies (id, name, domain,ticker) values (24,'Bank of Hoboken','www.bankofhb.com','');
+ALTER TABLE companies
+ADD ticker varchar(10);
+
+UPDATE companies SET ticker='WM' WHERE id='1';
+UPDATE companies SET ticker='EX' WHERE id='2';
+UPDATE companies SET ticker='CH' WHERE id='3';
+UPDATE companies SET ticker='BH' WHERE id='4';
+UPDATE companies SET ticker='AAPL' WHERE id='5';
